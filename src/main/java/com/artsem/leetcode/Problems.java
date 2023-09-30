76c901e3bede8aed1b30123ddbf24f84989d49c5
Artur Semenas <uncelfuka@gmail.com>
2023-09-30 18:53:01 +0200

    maxProduct refact (use sort)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -74,21 +74,23 @@ public class Problems {
     }
 
     public static int maxProduct(int[] nums) {
-        int max = 0;
-        int max2 = 0;
-        int index = 0;
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i]>max){
-                max=nums[i];
-                index = i;
-            }
-        }
-        for(int i = 0; i<nums.length; i++){
-            if(nums[i]>max2 && nums[i]<=max && i != index){
-                max2= nums[i];
-            }
-        }
-        return (max-1)*(max2-1);
+//        int max = 0;
+//        int max2 = 0;
+//        int index = 0;
+//        for(int i = 0; i<nums.length; i++){
+//            if(nums[i]>max){
+//                max=nums[i];
+//                index = i;
+//            }
+//        }
+//        for(int i = 0; i<nums.length; i++){
+//            if(nums[i]>max2 && nums[i]<=max && i != index){
+//                max2= nums[i];
+//            }
+//        }
+//        return (max-1)*(max2-1);
+         Arrays.sort(nums);
+         return (nums[nums.length-1]-1)*(nums[nums.length-2]-1);
     }
     public static String replaceDigits(String s) {
         StringBuilder sb = new StringBuilder ();

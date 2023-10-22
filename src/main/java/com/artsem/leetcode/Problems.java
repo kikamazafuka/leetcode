334c07b9c13f61827c393e5a1d0a164b452e413a
Artur Semenas <uncelfuka@gmail.com>
2023-10-22 16:30:44 +0200

    905. Sort Array By Parity

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -124,6 +124,25 @@ public class Problems {
     }
 
 
+    public int[] sortArrayByParity(int[] nums) {
+        if(nums.length<=1){
+            return nums;
+        }
+        int [] res = new int[nums.length];
+        int begin = 0;
+        int end = nums.length-1;
+        Arrays.sort(nums);
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i]%2==0){
+                res[begin]=nums[i];
+                begin++;
+            } else if(nums[i]%2!=0){
+                res[end]=nums[i];
+                end--;
+            }
+        }
+        return res;
+    }
     public int findNonMinOrMax(int[] nums) {
         if(nums.length<3){
             return -1;

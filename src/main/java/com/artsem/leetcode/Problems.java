c3b6e0ab25bb6ddeebed5ef2eb1cd4d933cf76ff
Artur Semenas <uncelfuka@gmail.com>
2023-10-29 18:43:23 +0100

    1374. Generate a String With Characters That Have Odd Counts

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -139,6 +139,16 @@ public class Problems {
     }
 
 
+    public String generateTheString(int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<n-1; i++){
+            sb.append("a");
+        }
+        if (n%2!=0){
+            sb.append("a");
+        } else sb.append("b");
+        return sb.toString();
+    }
     public int[] countBits(int n) {
         int [] res = new int [n+1];
         for(int i = 0; i<n+1; i++){

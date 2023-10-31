c6d8732551448764a94decc5ec2813eeeb59dee1
Artur Semenas <uncelfuka@gmail.com>
2023-11-01 00:23:08 +0100

    344. Reverse String

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -145,6 +145,13 @@ public class Problems {
     }
 
 
+    public void reverseString(char[] s) {
+        for(int i = 0, j = s.length-1; i<j; i++, j--){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+        }
+    }
     public static int findGCD(int[] nums) {
         int min = 1001;
         int max = 0;

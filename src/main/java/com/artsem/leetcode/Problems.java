8b39b0e81bec25c474a6190cd549c3a0c4592a02
Artur Semenas <uncelfuka@gmail.com>
2023-11-02 16:23:35 +0100

    1351. Count Negative Numbers in a Sorted Matrix

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -145,6 +145,17 @@ public class Problems {
     }
 
 
+    public int countNegatives(int[][] grid) {
+        int count = 0;
+        for(int i = 0; i<grid.length; i++){
+            for(int j = 0; j<grid[i].length; j++){
+                if(grid[i][j]<0){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
     public int[] findArray(int[] pref) {
         int []result = new int [pref.length];
         result[0] = pref[0];

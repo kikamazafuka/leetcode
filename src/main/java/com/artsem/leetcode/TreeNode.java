df462b9339cb1725ad9bae04a664393e3bff9b34
Artur Semenas <uncelfuka@gmail.com>
2023-12-28 23:02:54 +0100

    TreeNode class

--- src/main/java/com/artsem/leetcode/TreeNode.java
+++ src/main/java/com/artsem/leetcode/TreeNode.java
@@ -1,12 +1,12 @@
 package com.artsem.leetcode;
 
 public class TreeNode {
-    int val;
-    Problems.TreeNode left;
-    Problems.TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
     TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, Problems.TreeNode left, Problems.TreeNode right) {
+    public TreeNode(int val) { this.val = val; }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;

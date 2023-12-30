79f647ba3ff17cae77eb59eba477ce97dd17f401
Artur Semenas <uncelfuka@gmail.com>
2023-12-30 22:25:40 +0100

    UtilityClass

---
+++ src/main/java/com/artsem/leetcode/UtilityClass.java
@@ -0,0 +1,9 @@
+package com.artsem.leetcode;
+
+public class UtilityClass {
+
+    public static boolean isVowel(String c) {
+        String vowels = "aeiouAEIOU";
+        return vowels.contains(c);
+    }
+}

47cd83577b7ece3bd7628587c27f16443bcf7824
Artur Semenas <uncelfuka@gmail.com>
2024-01-03 00:26:54 +0100

    1137. N-th Tribonacci Number

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -87,6 +87,16 @@ public class Problems {
         System.out.println(Problems.guessNumber(5));
     }
 
+    public static int tribonacci(int n) {
+        int [] arr = new int [38];
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 1;
+        for(int i = 3; i<=n; i++){
+            arr[i] = arr[i-1] + arr[i-2] + arr[i-3];
+        }
+        return arr[n];
+    }
 
     public static int guessNumber(int n) {
         Random random = new Random();

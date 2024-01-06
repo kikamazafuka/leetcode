11e70210383790cf6f56fea82597735c342b5bfa
Artur Semenas <uncelfuka@gmail.com>
2024-01-06 23:11:41 +0100

    198. House Robber

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -87,6 +87,19 @@ public class Problems {
         System.out.println(Problems.guessNumber(5));
     }
 
+    public int rob(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        int dp[] = new int [nums.length+1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for(int i = 1; i<nums.length; i++){
+            dp[i+1] = Math.max(dp[i], dp[i-1] + nums[i]);
+        }
+        return dp[nums.length];
+    }
+
     public static int minCostClimbingStairs(int[] cost) {
 //        int n = cost.length;
 //        int[] dp = new int [n];

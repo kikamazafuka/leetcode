c919cd03001c577960adc75eb5e831455cc6b2fe
Artur Semenas <uncelfuka@gmail.com>
2024-01-12 23:21:31 +0100

    getUserChoice

--- src/main/java/com/artsem/leetcode/UtilityClass.java
+++ src/main/java/com/artsem/leetcode/UtilityClass.java
@@ -1,9 +1,23 @@
 package com.artsem.leetcode;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class UtilityClass {
 
     public static boolean isVowel(String c) {
         String vowels = "aeiouAEIOU";
         return vowels.contains(c);
+    }
+    public static int getUserChoice(){
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int choice;
+        try {
+            choice = Integer.parseInt(bufferedReader.readLine());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return choice;
     }
 }

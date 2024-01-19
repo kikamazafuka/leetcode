f1fea148647d384d18e6893961b71480af018c46
Artur Semenas <uncelfuka@gmail.com>
2024-01-19 23:28:49 +0100

    1880. Check if Word Equals Summation of Two Words(R:100%M:43%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -87,6 +87,15 @@ public class Problems {
         System.out.println(Problems.guessNumber(5));
     }
 
+    public boolean isSumEqual2(String firstWord, String secondWord, String targetWord) {
+        return getIntValue(firstWord) + getIntValue(secondWord) == getIntValue(targetWord);
+    }
+    private int getIntValue(String s) {
+        int result = 0;
+        for(int i=0;i<s.length();i++)
+            result = result * 10 + (s.charAt(i) - 'a');
+        return result;
+    }
     public static boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
 
         StringBuilder sNum1 = new StringBuilder();

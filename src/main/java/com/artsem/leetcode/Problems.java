0b05a58e7fae532b61df72c1494513180f95d34e
Artur Semenas <uncelfuka@gmail.com>
2024-01-22 23:26:34 +0100

    2351. First Letter to Appear Twice(R:100%M:12%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -87,6 +87,16 @@ public class Problems {
         System.out.println(Problems.guessNumber(5));
     }
 
+    public char repeatedCharacterS(String s) {
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i<s.length();i++){
+            if(set.contains(s.charAt(i))){
+                return s.charAt(i);
+            }
+            set.add(s.charAt(i));
+        }
+        throw new RuntimeException("s should contain at least one repeated letter");
+    }
     public char repeatedCharacter(String s) {
         Set<Character> set = new HashSet<>();
         for(char c : s.toCharArray()){

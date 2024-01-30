e4a55b83604fc0c48c543301cef47ccbdaa87e32
Artur Semenas <uncelfuka@gmail.com>
2024-01-30 23:27:26 +0100

    1876. Substrings of Size Three with Distinct Characters (R:90%,M:47%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -88,6 +88,20 @@ public class Problems {
         System.out.println(Problems.guessNumber(5));
     }
 
+    public int countGoodSubstrings(String s) {
+        if(s.length()<3){
+            return 0;
+        }
+        int count = 0;
+        for(int i = 2; i<s.length(); i++){
+            if(s.charAt(i)!=s.charAt(i-1) && s.charAt(i)!=s.charAt(i-2)
+                    && s.charAt(i-1)!=s.charAt(i-2)){
+                count++;
+            }
+        }
+        return count;
+    }
+
     public String maximumOddBinaryNumber(String s) {
         StringBuilder sb = new StringBuilder();
         int count1 = 0;

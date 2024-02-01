95b80b663589fc922d538dcf81808564dabacc00
Artur Semenas <uncelfuka@gmail.com>
2024-02-01 21:24:21 +0100

    2788. Split Strings by Separator (R:13%,M:6%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -88,6 +88,22 @@ public class Problems {
         System.out.println(Problems.guessNumber(5));
     }
 
+    public List<String> splitWordsBySeparatorArr(List<String> words, char separator) {
+        List<String> nm=new ArrayList<>();
+        for(int i=0;i<words.size();i++)
+        {
+            String arr[]=words.get(i).split("["+separator+"]");
+            for(int j=0;j<arr.length;j++)
+            {
+
+                if(arr[j].length()>0)
+                {
+                    nm.add(arr[j]);
+                }
+            }
+        }
+        return nm;
+    }
     public static List<String> splitWordsBySeparator(List<String> words, char separator) {
         List<String> list = new ArrayList<>();
         for(String s : words){

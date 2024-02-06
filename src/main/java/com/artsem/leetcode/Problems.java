0a2a6262775655628a6bdfb1ef3760d51d5724d3
Artur Semenas <uncelfuka@gmail.com>
2024-02-06 21:38:01 +0100

    2057. Smallest Index With Equal Value (R:100%,M:62%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -90,6 +90,14 @@ public class Problems {
         System.out.println(Problems.guessNumber(5));
     }
 
+    public int smallestEqual(int[] nums) {
+        for(int i = 0; i<nums.length; i++){
+            if(i%10 == nums[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
     public static int countPrefixesStartWith(String[] words, String s) {
         int count = 0;
 

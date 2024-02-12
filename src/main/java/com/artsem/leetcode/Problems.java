ee4ab3bd5ecfefda2ed1f8de37840816750845ad
Artur Semenas <uncelfuka@gmail.com>
2024-02-12 22:15:26 +0100

    496. Next Greater Element I (R:89%,M:66%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -90,6 +90,18 @@ public class Problems {
         System.out.println(Problems.guessNumber(5));
     }
 
+    public int[] nextGreaterElementMap(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums2) {
+            while (!stack.isEmpty() && stack.peek() < num)
+                map.put(stack.pop(), num);
+            stack.push(num);
+        }
+        for (int i = 0; i < nums1.length; i++)
+            nums1[i] = map.getOrDefault(nums1[i], -1);
+        return nums1;
+    }
     public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int [] res = new int[nums1.length];
         boolean equal = false;

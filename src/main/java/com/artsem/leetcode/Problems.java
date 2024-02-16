4b5b7ba1861c46e8f3ab00790f8e67bfaa1593da
Artur Semenas <uncelfuka@gmail.com>
2024-02-16 21:09:03 +0100

    1217. Minimum Cost to Move Chips to The Same Position (R:100%,M:33%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -90,6 +90,16 @@ public class Problems {
         System.out.println(Problems.guessNumber(5));
     }
 
+    public int minCostToMoveChips(int[] position) {
+        int even = 0;
+        int odd = 0;
+        for(int n : position){
+            if(n%2==0){
+                even++;
+            } else odd++;
+        }
+        return Math.min(even, odd);
+    }
     public static int[] sortedSquares2Pointers(int[] nums) {
         int [] res = new int [nums.length];
         int n = nums.length;

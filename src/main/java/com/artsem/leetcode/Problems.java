0cd7a0b67c28fcedea1ea50a4e20958255589d10
Artur Semenas <uncelfuka@gmail.com>
2024-03-09 21:38:59 +0100

    867. Transpose Matrix (R:29%,M:53%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -35,6 +35,17 @@ public class Problems {
 
     }
 
+    public int[][] transpose(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int [][] res = new int [n][m];
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                res[j][i] = matrix[i][j];
+            }
+        }
+        return res;
+    }
     public static int longestCommonSubsequence(String text1, String text2) {
         int n = text1.length();
         int m = text2.length();

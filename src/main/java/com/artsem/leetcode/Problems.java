0c6d3501e7cd99dd6970ff8e10bfd4f3ff4031f7
Artur Semenas <uncelfuka@gmail.com>
2024-03-17 20:44:32 +0100

    922. Sort Array By Parity II (R:99%,M:87%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -35,6 +35,22 @@ public class Problems {
 
     }
 
+    public int[] sortArrayByParityII(int[] nums) {
+        int [] res = new int [nums.length];
+        int odd = 1;
+        int even = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i]%2==0){
+                res[even] = nums[i];
+                even += 2;
+            } else{
+                res[odd] = nums[i];
+                odd += 2;
+            }
+        }
+        return res;
+    }
+
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()){
             return false;

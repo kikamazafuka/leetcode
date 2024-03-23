0891b50474fcf909a19d3179f37a1d1e0bafa588
Artur Semenas <uncelfuka@gmail.com>
2024-03-23 22:13:02 +0100

    2399. Check Distances Between Same Letters (R:93%,M:58%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -35,6 +35,21 @@ public class Problems {
 
     }
 
+    public boolean checkDistancesHash(String s, int[] distance) {
+        Map<Character,Integer> map = new HashMap<>();
+        char[] arr = s.toCharArray();
+        int len = arr.length;
+
+        for(int i=0; i<len; i++){
+            if(map.containsKey(arr[i])){
+                int start = map.get(arr[i]);
+                int diff = i - start - 1;
+                if(distance[arr[i]-'a'] != diff) return false;
+            }
+            else map.put(arr[i], i);
+        }
+        return true;
+    }
     public static boolean checkDistances(String s, int[] distance) {
         Map<Character, Integer> map = new HashMap<>();
         Set<Character> set = new HashSet<>();

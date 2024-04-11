9051a4f38162ff6e415eb166a055a7612126ec97
Artur Semenas <uncelfuka@gmail.com>
2024-04-12 00:00:18 +0200

    1200. Minimum Absolute Difference (R:72%,M:83%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -28,15 +28,16 @@ public class Problems {
 
     public static List<List<Integer>> minimumAbsDifference(int[] arr) {
         Arrays.sort(arr);
-        int minDif = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE;
         List<List<Integer>> res = new ArrayList<>();
         for(int i = 0; i<arr.length-1; i++){
-            minDif = Math.min(minDif, arr[i+1]-arr[i]);
-        }
-        for(int i = 0; i<arr.length-1; i++){
-            int dif = arr[i+1] - arr[i];
-            if(dif==minDif){
-                res.add(new ArrayList<>(Arrays.asList(arr[i],arr[i+1])));
+            int diff = arr[i+1] - arr[i];
+            if (diff < min) {
+                min = diff;
+                res.clear();
+                res.add(Arrays.asList(arr[i], arr[i + 1]));
+            } else if (diff == min) {
+                res.add(Arrays.asList(arr[i], arr[i + 1]));
             }
         }
         return res;

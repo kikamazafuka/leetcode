2abe62f69a479231a4ae364cb9a8f34f0d1cf43c
Artur Semenas <uncelfuka@gmail.com>
2024-04-11 23:48:16 +0200

    1200. Minimum Absolute Difference (R:-%,M:-%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -26,6 +26,21 @@ public class Problems {
         GregorianCalendar gregorianCalendar = new GregorianCalendar();
     }
 
+    public static List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        int minDif = 0;
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i<arr.length-1; i++){
+            minDif = Math.min(minDif, arr[i+1]-arr[i]);
+        }
+        for(int i = 0; i<arr.length-1; i++){
+            int dif = arr[i+1] - arr[i];
+            if(dif==minDif){
+                res.add(new ArrayList<>(Arrays.asList(arr[i],arr[i+1])));
+            }
+        }
+        return res;
+    }
     public long findTheArrayConcValString(int[] nums) {
         long res = 0;
         for (int i = 0, j = nums.length - 1; i <= j; i++, j--) {

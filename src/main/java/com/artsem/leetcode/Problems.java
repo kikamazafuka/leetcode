4b2565195fb1aba94842a5e091ad529f792ed6d5
Artur Semenas <uncelfuka@gmail.com>
2024-04-15 23:09:17 +0200

    283. Move Zeroes (R:85%,M:45%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -26,6 +26,17 @@ public class Problems {
         GregorianCalendar gregorianCalendar = new GregorianCalendar();
     }
 
+    public void moveZeroes(int[] nums) {
+        int k = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i]!=0){
+                int temp = nums[k];
+                nums[k] = nums[i];
+                nums[i] = temp;
+                k++;
+            }
+        }
+    }
     public static int minSubArrayLenSlidWin(int target, int[] nums) {
         int sum = 0;
         int k = 0;

41419205578587c0961b531595da1bb26518feb1
Artur Semenas <uncelfuka@gmail.com>
2024-04-16 23:55:44 +0200

    48. Rotate Image (R:100%,M:90%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -26,6 +26,23 @@ public class Problems {
         GregorianCalendar gregorianCalendar = new GregorianCalendar();
     }
 
+    public void rotate(int[][] matrix) {
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = i+1; j<matrix[0].length; j++){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = 0; j<matrix[0].length/2; j++){
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = tmp;
+            }
+        }
+    }
     public void moveZeroes(int[] nums) {
         int k = 0;
         for(int i = 0; i<nums.length; i++){

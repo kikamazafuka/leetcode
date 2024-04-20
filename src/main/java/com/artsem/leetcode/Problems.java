8ece1802e2bac4abf8f0b8375a6627cff2618f07
Artur Semenas <uncelfuka@gmail.com>
2024-04-21 00:07:03 +0200

    965. Univalued Binary Tree (R:100%,M:40%) iterative

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -26,6 +26,19 @@ public class Problems {
     }
 
     public boolean isUnivalTree(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode n = q.poll();
+            if(n.val != root.val){
+                return false;
+            }
+            if (n.left!=null) q.offer(n.left);
+            if (n.right!=null) q.offer(n.right);
+        }
+        return true;
+    }
+    public boolean isUnivalTreeRecurs(TreeNode root) {
         Set<Integer> set = new HashSet<>();
         UtilityClass.dfs(root, set);
         return set.size() == 1;

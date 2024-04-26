aa9a4aa7286455646aa3ee2924b8fb54360ce3d3
Artur Semenas <uncelfuka@gmail.com>
2024-04-27 00:02:19 +0200

    3120. Count the Number of Special Characters I (R:100%,M:78%)

--- src/main/java/com/artsem/leetcode/easy/CountTheNumberOfSpecialCharactersI.java
+++ src/main/java/com/artsem/leetcode/easy/CountTheNumberOfSpecialCharactersI.java
@@ -1,9 +1,30 @@
 package com.artsem.leetcode.easy;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class CountTheNumberOfSpecialCharactersI {
+    public int numberOfSpecialCharsArr(String word) {
+        int res = 0;
+        int [] a = new int[26];
+        int [] b = new int[26];
+        for(char c : word.toCharArray()){
+            if (c>='a' && c<='z'){
+                a[c - 'a']++;
+            } else {
+                b[c - 'A']++;
+            }
+        }
+        for (int i = 0; i<a.length; i++){
+            if (a[i] != 0 && b[i] != 0){
+                res++;
+            }
+        }
+        return res;
+    }
+
     public int numberOfSpecialChars(String word) {
         int res = 0;
         Set<Character> set = new HashSet<>();

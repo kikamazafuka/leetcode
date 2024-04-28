ee8675859642f08590a2e66bee041bdac52ec5d6
Artur Semenas <uncelfuka@gmail.com>
2024-04-28 22:03:49 +0200

    3079. Find the Sum of Encrypted Integers (R:99%,M:69%)

--- src/main/java/com/artsem/leetcode/easy/FindTheSumOfEncryptedIntegers.java
+++ src/main/java/com/artsem/leetcode/easy/FindTheSumOfEncryptedIntegers.java
@@ -3,6 +3,23 @@ package com.artsem.leetcode.easy;
 import java.util.Arrays;
 
 public class FindTheSumOfEncryptedIntegers {
+
+    public int sumOfEncryptedIntRef(int[] nums) {
+        int res = 0;
+        for(int i = 0; i<nums.length; i++){
+            int maxDigit = 0;
+            int newNum = 0;
+            while(nums[i]>0){
+                int digit = nums[i] % 10;
+                maxDigit = Math.max(maxDigit, digit);
+                nums[i] /= 10;
+                newNum = newNum * 10 + 1;
+            }
+            newNum *= maxDigit;
+            res += newNum;
+        }
+        return res;
+    }
     public static int sumOfEncryptedInt(int[] nums) {
         for(int i = 0; i<nums.length; i++){
             int maxDigit = 0;

8cd8d6d42a8108a54599151f1c132090b6015824
Artur Semenas <uncelfuka@gmail.com>
2024-04-28 21:27:26 +0200

    3090. Maximum Length Substring With Two Occurrences (R:60%,M:94%)

--- src/main/java/com/artsem/leetcode/easy/MaximumLengthSubstringWithTwoOccurrences.java
+++ src/main/java/com/artsem/leetcode/easy/MaximumLengthSubstringWithTwoOccurrences.java
@@ -10,15 +10,11 @@ public class MaximumLengthSubstringWithTwoOccurrences {
         int res = 0;
         int currLen = 0;
         for(int i = 0; i<s.length(); i++){
-            char c = s.charAt(i);
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0)+1);
-
-            if(map.get(s.charAt(i)) > 2){
-                while (map.get(s.charAt(i))>2){
-                    map.put(s.charAt(l), map.get(s.charAt(l))-1);
-                    l++;
-                    currLen--;
-                }
+            while (map.get(s.charAt(i))>2){
+                map.put(s.charAt(l), map.get(s.charAt(l))-1);
+                l++;
+                currLen--;
             }
             currLen++;
             res = Math.max(res, currLen);

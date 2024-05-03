573acc76126a8f495c8db048eab163755627f527
Artur Semenas <uncelfuka@gmail.com>
2024-05-03 18:42:23 +0200

    3131. Find the Integer Added to Array I (R:71% M:99%)

--- src/main/java/com/artsem/leetcode/Problems.java
+++ src/main/java/com/artsem/leetcode/Problems.java
@@ -31,7 +31,17 @@ public class Problems {
         GregorianCalendar gregorianCalendar = new GregorianCalendar();
     }
 
-    public static int addedInteger(int[] nums1, int[] nums2) {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        for(int i = 0; i<nums1.length; i++){
+            min1 = Math.min(min1, nums1[i]);
+            min2 = Math.min(min2, nums2[i]);
+        }
+        return min2 - min1;
+    }
+
+    public static int addedIntegerSort(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         return nums2[0] - nums1[0];

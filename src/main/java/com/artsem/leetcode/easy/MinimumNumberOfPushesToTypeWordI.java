96822fc6bc0894933d8041b3579481bd66f58603
Artur Semenas <uncelfuka@gmail.com>
2024-05-10 18:19:38 +0200

    3014. Minimum Number of Pushes to Type Word I (R:100% M:87%)

--- src/main/java/com/artsem/leetcode/easy/MinimumNumberOfPushesToTypeWordI.java
+++ src/main/java/com/artsem/leetcode/easy/MinimumNumberOfPushesToTypeWordI.java
@@ -4,14 +4,14 @@ public class MinimumNumberOfPushesToTypeWordI {
     public static int minimumPushes(String word) {
         int res = 0;
         int incr = 1;
-        int count = 1;
+        int count = 0;
         for(int i = 0; i<word.length(); i++){
-            if(count > 8){
+            count++;
+            res += incr;
+            if(count >= 8){
                 incr++;
                 count = 0;
             }
-            count++;
-            res += incr;
         }
         return res;
     }

3b8880f75943fbb94ff35a43702291323f0f30dd
Artur Semenas <uncelfuka@gmail.com>
2024-05-12 19:55:34 +0200

    3010. Divide an Array Into Subarrays With Minimum Cost I (R:95% M:53%)

--- src/main/java/com/artsem/leetcode/easy/DivideAnArrayIntoSubarraysWithMinimumCostI.java
+++ src/main/java/com/artsem/leetcode/easy/DivideAnArrayIntoSubarraysWithMinimumCostI.java
@@ -1,6 +1,23 @@
 package com.artsem.leetcode.easy;
 
 public class DivideAnArrayIntoSubarraysWithMinimumCostI {
+
+    public int minimumCostOneLoop(int[] nums) {
+        int res = nums[0];
+        int min = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        int index = 0;
+        for(int i = 1; i<nums.length; i++){
+            if(nums[i]<min) {
+                min2 = min;
+                min = nums[i];
+            } else if(nums[i]<min2){
+                min2 = nums[i];
+            }
+        }
+        res += min + min2;
+        return res;
+    }
     public int minimumCost(int[] nums) {
         int res = nums[0];
         int min = Integer.MAX_VALUE;

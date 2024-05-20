55fedabc62f74582e8ca0ee1dfd364d6bb3ab834
Artur Semenas <uncelfuka@gmail.com>
2024-05-20 21:47:58 +0200

    3151. Special Array I (R:100% M:37%)

--- src/main/java/com/artsem/leetcode/easy/SpecialArrayI.java
+++ src/main/java/com/artsem/leetcode/easy/SpecialArrayI.java
@@ -10,4 +10,11 @@ public class SpecialArrayI {
         }
         return true;
     }
+
+    public boolean isArraySpecialRef(int[] nums) {
+        for(int i = 0; i<nums.length-1; i++){
+            if(nums[i]%2==nums[i+1]%2)return false;
+        }
+        return true;
+    }
 }

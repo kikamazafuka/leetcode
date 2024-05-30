7f7693276a8bc9f029381b2887c46fd3aafc2a1b
Artur Semenas <uncelfuka@gmail.com>
2024-05-31 00:13:31 +0200

    2917. Find the K-or of an Array (R:36% M:12%)

--- src/main/java/com/artsem/leetcode/easy/FindTheKorOfAnArray.java
+++ src/main/java/com/artsem/leetcode/easy/FindTheKorOfAnArray.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FindTheKorOfAnArray {
-    public static int findKOr(int[] nums, int k) {
+    public static int findKOrMap(int[] nums, int k) {
 
         Map<Integer,Integer> map = new HashMap<>();
         for(int i = 0; i<nums.length; i++){
@@ -23,6 +23,32 @@ public class FindTheKorOfAnArray {
             if (entry.getValue()>=k){
                 res[entry.getKey()] = 1;
             }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = res.length-1; i>=0; i--){
+            sb.append(res[i]);
+        }
+        return Integer.valueOf(sb.toString(),2);
+    }
+
+    public static int findKOr(int[] nums, int k) {
+
+        int [] res = new int [32];
+        for(int i = 0; i<nums.length; i++){
+            int bit = 0;
+            while(nums[i]>0){
+                int currBit = nums[i]&1;
+                if(currBit==1){
+                   res[bit]++;
+                }
+                bit++;
+                nums[i] >>= 1;
+            }
+        }
+        for(int i = 0; i<res.length; i++){
+            if(res[i]>=k){
+                res[i] = 1;
+            } else res[i] = 0;
         }
         StringBuilder sb = new StringBuilder();
         for (int i = res.length-1; i>=0; i--){

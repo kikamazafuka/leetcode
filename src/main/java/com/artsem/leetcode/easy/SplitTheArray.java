cc0d5f70645c3bef3304ce1e482c1d7b0f4695ff
Artur Semenas <uncelfuka@gmail.com>
2024-06-06 21:36:01 +0200

    3046. Split the Array (R:96% M:66%)

--- src/main/java/com/artsem/leetcode/easy/SplitTheArray.java
+++ src/main/java/com/artsem/leetcode/easy/SplitTheArray.java
@@ -4,6 +4,17 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SplitTheArray {
+
+    public boolean isPossibleToSplitArr(int[] nums) {
+        int [] res = new int [101];
+        for(int i = 0; i<nums.length; i++){
+            res[nums[i]]++;
+        }
+        for (int val : res){
+            if (val>2)return false;
+        }
+        return true;
+    }
     public boolean isPossibleToSplit(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i<nums.length; i++){

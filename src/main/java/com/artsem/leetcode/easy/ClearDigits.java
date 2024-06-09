d016a7cfb82dfeef28b1c4f3460d485ec5971c54
Artur Semenas <uncelfuka@gmail.com>
2024-06-09 17:50:01 +0200

    3174. Clear Digits (R:100% M:100%)

--- src/main/java/com/artsem/leetcode/easy/ClearDigits.java
+++ src/main/java/com/artsem/leetcode/easy/ClearDigits.java
@@ -5,23 +5,29 @@ import java.util.Set;
 
 public class ClearDigits {
     public static String clearDigits(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        for(int i = 1; i<s.length(); i++){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<s.length(); i++){
             if(Character.isDigit(s.charAt(i))){
-                sb.deleteCharAt(i);
-                sb.deleteCharAt(i-1);
-            }
+                sb.deleteCharAt(sb.length()-1);
+            }else sb.append(s.charAt(i));
         }
         return sb.toString();
     }
 
-    public String clearDigitsSet(String s) {
+    public static String clearDigitsSet(String s) {
+        StringBuilder sb = new StringBuilder();
         Set<Integer> set = new HashSet<>();
         for(int i = 1; i<s.length();i++){
             if(Character.isDigit(s.charAt(i))){
                 set.add(i-1);
             }
         }
-        for(int i = 0; )
+        for(int i = 0; i<s.length(); i++){
+            if (set.contains(i) || Character.isDigit(s.charAt(i))){
+                continue;
+            }
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
     }
 }

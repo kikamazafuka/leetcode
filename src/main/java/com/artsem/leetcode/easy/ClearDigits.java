03845ce23535f717ca39b3acfdf5fa3a1b22aaaa
Artur Semenas <uncelfuka@gmail.com>
2024-06-11 22:16:47 +0200

    3174. Clear Digits (R:85% M:91%)

--- src/main/java/com/artsem/leetcode/easy/ClearDigits.java
+++ src/main/java/com/artsem/leetcode/easy/ClearDigits.java
@@ -8,7 +8,6 @@ public class ClearDigits {
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i<s.length(); i++){
             if(Character.isDigit(s.charAt(i))){
-                //if(s.charAt(i)>='0' && s.charAt(i)<='9'){
                 sb.deleteCharAt(sb.length()-1);
             }else {
                 sb.append(s.charAt(i));

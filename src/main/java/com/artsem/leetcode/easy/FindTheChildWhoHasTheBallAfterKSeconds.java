a0282aa518a5906085a22056ae6f63e21659e999
Artur Semenas <uncelfuka@gmail.com>
2024-06-15 22:06:48 +0200

    3178. Find the Child Who Has the Ball After K Seconds (R:100% M:85%)

--- src/main/java/com/artsem/leetcode/easy/FindTheChildWhoHasTheBallAfterKSeconds.java
+++ src/main/java/com/artsem/leetcode/easy/FindTheChildWhoHasTheBallAfterKSeconds.java
@@ -26,4 +26,18 @@ public class FindTheChildWhoHasTheBallAfterKSeconds {
 
         return (k / --n & 1) > 0 ? n - k % n : k % n;
     }
+
+    public int numberOfChildIE(int n, int k) {
+        n--; // Decrement n to simplify calculation (so range is now 0 to n-1)
+        int rounds = k / n; // Calculate the number of complete back-and-forth trips
+        int rem = k % n; // Calculate the remaining steps after the last complete trip
+
+        if (rounds % 2 == 0) {
+            // If the number of complete back-and-forth trips is even
+            return rem; // The ball is passed forward from the start
+        } else {
+            // If the number of complete back-and-forth trips is odd
+            return n - rem; // The ball is passed backward from the end
+        }
+    }
 }

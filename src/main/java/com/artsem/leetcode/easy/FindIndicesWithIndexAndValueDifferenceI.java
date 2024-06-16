29e6e494d4bcac16730ee344977a2bfdf41017af
Artur Semenas <uncelfuka@gmail.com>
2024-06-16 21:21:31 +0200

    2903. Find Indices With Index and Value Difference I (R:98% M:22%) one pass

--- src/main/java/com/artsem/leetcode/easy/FindIndicesWithIndexAndValueDifferenceI.java
+++ src/main/java/com/artsem/leetcode/easy/FindIndicesWithIndexAndValueDifferenceI.java
@@ -11,4 +11,14 @@ public class FindIndicesWithIndexAndValueDifferenceI {
         }
         return new int []{-1,-1};
     }
+    public int[] findIndicesOnePass(int[] A, int d, int v) {
+        int mini = 0, maxi = 0, n = A.length;
+        for (int i = d; i < n; i++) {
+            if (A[i - d] < A[mini]) mini = i - d;
+            if (A[i - d] > A[maxi]) maxi = i - d;
+            if (A[i] - A[mini] >= v) return new int[] {mini, i};
+            if (A[maxi] - A[i] >= v) return new int[] {maxi, i};
+        }
+        return new int[] { -1, -1};
+    }
 }

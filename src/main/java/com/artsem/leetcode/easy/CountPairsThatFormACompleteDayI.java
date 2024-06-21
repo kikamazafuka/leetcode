26f78e2c4e9fd1a466e669914163a7750f4d2fa7
Artur Semenas <uncelfuka@gmail.com>
2024-06-21 21:20:28 +0200

    3184. Count Pairs That Form a Complete Day I (R:7% M:96%)

--- src/main/java/com/artsem/leetcode/easy/CountPairsThatFormACompleteDayI.java
+++ src/main/java/com/artsem/leetcode/easy/CountPairsThatFormACompleteDayI.java
@@ -6,7 +6,7 @@ public class CountPairsThatFormACompleteDayI {
         for(int i = 0; i<hours.length; i++){
             for(int j = i+1; j<hours.length; j++){
                 int currHours = hours[i]+hours[j];
-                if(currHours>=24 && currHours%24==0){
+                if(currHours%24==0){
                     res++;
                 }
             }

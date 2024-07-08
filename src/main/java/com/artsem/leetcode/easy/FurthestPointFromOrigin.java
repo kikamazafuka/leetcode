dd79291b2d4ed8942efbbadd646ae2da8d6f432f
Artur Semenas <uncelfuka@gmail.com>
2024-07-08 22:26:14 +0200

    2833. Furthest Point From Origin (R:100% M:42%)

--- src/main/java/com/artsem/leetcode/easy/FurthestPointFromOrigin.java
+++ src/main/java/com/artsem/leetcode/easy/FurthestPointFromOrigin.java
@@ -15,4 +15,14 @@ public class FurthestPointFromOrigin {
         int temp = Math.abs(countL-countR);
         return temp+countSpace;
     }
+
+    public int furthestDistanceFromOriginR(String moves) {
+        int res = 0, d = 0;
+        for (int i = 0; i < moves.length(); ++i) {
+            if (moves.charAt(i) == 'L') res--;
+            if (moves.charAt(i) == 'R') res++;
+            if (moves.charAt(i) == '_') d++;
+        }
+        return Math.abs(res) + d;
+    }
 }
